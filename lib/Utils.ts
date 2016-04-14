47130daf15b6ec480439f77b8ff47e10ed29f7ad
ARASHI, Jumpei <jumpei.arashi@arashike.com>
2016-04-14 18:39:30 +0900

    trim ""

--- lib/Utils.ts
+++ lib/Utils.ts
@@ -1,11 +1,11 @@
 import * as _ from 'lodash';
 
 export function csv2json(csv:string): Object[] {
-  const headers = csv.split('\n')[0].split(',');
+  const headers = csv.split('\n')[0].split(',').map(header => {return _.trim(header, '"')});
   const lines = _.tail(csv.split('\n'));
   return lines.map(line => {
-    let values = line.split(',');
-    values = values.map(value => {return value === '""' ? null : value;});
+    let values = _.map(line.split(','), value => {return _.trim(value, '"')});
+    values = values.map(value => {return value.length === 0 ? null : value;});
     return _.zipObject(headers, values);
   });
 }
